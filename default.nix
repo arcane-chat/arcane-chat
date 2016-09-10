6425056055e0b218682583f2cacb762dfef9bc85
Remy Goldschmidt <taktoa@gmail.com>
2016-09-10 10:24:15 -0500

    Added rtags wrapper and fixed minor bug in iwyu

--- default.nix
+++ default.nix
@@ -13,6 +13,33 @@ let
         patches = [ ./toxcore.patch ];
       });
 
+      rtags = pkgs.stdenv.mkDerivation {
+        name = pkgs.rtags.name;
+        src = pkgs.rtags;
+        buildInputs = [ pkgs.makeWrapper ];
+        buildPhase = "";
+        installPhase = ''
+            VERSION="${pkgs.gcc.cc.version}"
+            SYSTEM="$(basename $(dirname $(dirname $(${pkgs.gcc.cc}/bin/g++ -print-libgcc-file-name))))"
+            FLAGS=""
+            FLAGS="$FLAGS -isystem ${pkgs.gcc.libc}/include"
+            FLAGS="$FLAGS -isystem ${pkgs.gcc.cc}/include"
+            FLAGS="$FLAGS -isystem ${pkgs.gcc.cc}/include/c++/$VERSION"
+            FLAGS="$FLAGS -isystem ${pkgs.gcc.cc}/include/c++/$VERSION/$SYSTEM"
+            FLAGS="$FLAGS -isystem ${pkgs.gcc.cc}/include/c++/$VERSION/backward"
+            FLAGS="$FLAGS -isystem ${pkgs.gcc.cc}/lib/gcc/$SYSTEM/$VERSION/include"
+            FLAGS="$FLAGS -isystem ${pkgs.gcc.cc}/lib/gcc/$SYSTEM/$VERSION/include-fixed"
+            FLAGS="$FLAGS \$NIX_CFLAGS_COMPILE"
+            mkdir -p $out/bin
+            cp ./bin/rdm $out/bin/rdm
+            cp ./bin/rp $out/bin/rp
+            cp ./bin/rc $out/bin/rc
+            cp -R ./share $out/
+            wrapProgram $out/bin/rdm \
+                --add-flags "\$(echo $FLAGS | sed 's/-isystem/--isystem/g')"
+        '';
+      };
+
       include-what-you-use = pkgs.include-what-you-use.overrideDerivation (old: {
         name = "include-what-you-use-3.8";
 
@@ -28,7 +55,7 @@ let
             patchShebangs $out/bin
             ln -s ${pkgs.llvmPackages_38.clang-unwrapped}/lib $out/lib
             VERSION="${pkgs.gcc.cc.version}"
-            SYSTEM="$(basename $(dirname $(dirname $(g++ -print-libgcc-file-name))))"
+            SYSTEM="$(basename $(dirname $(dirname $(${pkgs.gcc.cc}/bin/g++ -print-libgcc-file-name))))"
             FLAGS=""
             FLAGS="$FLAGS -isystem ${pkgs.gcc.libc}/include"
             FLAGS="$FLAGS -isystem ${pkgs.gcc.cc}/include"
