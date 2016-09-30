1d72c310f5809cf93a6b73ff15b51a2548f1321d
michael bishop <cleverca22@gmail.com>
2016-09-30 04:03:35 -0300

    pre-build wine and windows cross-builds

--- release.nix
+++ release.nix
@@ -3,6 +3,7 @@
 let default = import ./default.nix { inherit nixpkgs; };
 in {
   arcane-chat.linux = default.linux.arcane-chat;
-  # windows = default.windows.arcane-chat;
+  wineWow-linux = default.linux.super.wine.override { wineBuild = "wineWow"; };
+  arcane-chat-windows = default.windows.arcane-chat.crossDrv;
   # darwin = default.darwin.arcane-chat;
 }
