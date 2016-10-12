705104f6635c76ab27bf89df11afacf42060d999
Remy Goldschmidt <taktoa@gmail.com>
2016-10-12 01:13:38 -0500

    refactored chat-shaker/default.nix, uses ghc-8.0 now

--- chat-shaker/default.nix
+++ chat-shaker/default.nix
@@ -1,10 +1,9 @@
-{ runCommand, haskell }:
+{ runCommand, haskellPackages }:
 
-let
-  paths = p: with p; [ shake shake-language-c ];
-  ghc = haskell.packages.ghc7103.ghcWithPackages paths;
+let paths = p: with p; [ shake shake-language-c ];
+    ghc = haskellPackages.ghcWithPackages paths;
 in runCommand "chat-shaker" { buildInputs = [ ghc ]; } ''
-  mkdir -pv $out/bin
-  cp -vi ${./chat-shaker.hs} chat-shaker.hs
-  ghc chat-shaker.hs -o $out/bin/chat-shaker
+    mkdir -pv $out/bin
+    cp -vi ${./chat-shaker.hs} chat-shaker.hs
+    ghc chat-shaker.hs -o $out/bin/chat-shaker
 ''
