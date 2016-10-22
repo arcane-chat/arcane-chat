c010b57f9cb209bd2a8799c3e511e905ab7bdb5c
Remy Goldschmidt <taktoa@gmail.com>
2016-10-22 10:14:05 -0500

    Mostly purged CMake from project

--- release.nix
+++ release.nix
@@ -7,15 +7,14 @@ let
     ${x} = {
       windows = (lib.attrByPath (lib.splitString "." x) null default.windows).crossDrv;
       linux = lib.attrByPath (lib.splitString "." x) null default.linux;
+      # FIXME: darwin
     };
   };
   makeJobs = lib.foldl (total: next: total // (makeJob next)) {};
 in {
   wineWow-linux = default.linux.super.wine.override { wineBuild = "wineWow"; };
   chat-shaker = default.linux.super.chat-shaker;
-  arcane-chat.linux = default.linux.arcane-chat;
   chat-doc = default.linux.super.chat-doc;
-  # darwin = default.darwin.arcane-chat;
 } // makeJobs [
   "super.arcane-chat"
   "super.gst_all_1.gstreamermm"
@@ -23,4 +22,4 @@ in {
   "super.qt56.qtbase"
   "super.qt56.qtdeclarative"
   "super.x264"
-  ]
+]
